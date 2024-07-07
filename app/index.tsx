f6bb9dd952d486fe15355c13f07b732a7bc3b5a9
RizWaaN3024 <rizwaanans786@gmail.com>
2024-07-08 03:27:02 +0530

    Added Layout screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,19 @@
-import { Image, StyleSheet, Platform, View, Text } from "react-native";
+import { View, Text, ImageBackground } from "react-native";
+import React from "react";
+import beachImage from "@/assets/meditation-images/beach.webp";
 
-export default function HomeScreen() {
-  <View className="flex-1 justify-center items-center">
-    <Text>Hello React Native</Text>
-  </View>;
-}
+const App = () => {
+  return (
+    <View className="flex-1">
+      <ImageBackground
+        source={beachImage}
+        resizeMode="cover"
+        className="flex-1"
+      >
+        <Text className="text-white">App</Text>
+      </ImageBackground>
+    </View>
+  );
+};
+
+export default App;
