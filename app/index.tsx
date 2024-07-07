f6fa2fff0ef6a9e23c2c4d2b77a834fe261afb25
RizWaaN3024 <rizwaanans786@gmail.com>
2024-07-08 03:12:55 +0530

    Nativewind Setup

--- app/index.tsx
+++ app/index.tsx
@@ -1,18 +1,7 @@
 import { Image, StyleSheet, Platform, View, Text } from "react-native";
 
 export default function HomeScreen() {
-  <View style={styles.container}>
-    <Text style={styles.title}>Hello React Native</Text>
+  <View className="flex-1 justify-center items-center">
+    <Text>Hello React Native</Text>
   </View>;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  title: {
-    color: "white",
-  },
-});
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./App.{js,jsx,ts,tsx}",
+    "./app/**/*.{js,jsx,ts,tsx}", // Include all JS, JSX, TS, and TSX files in the app folder
+    "./components/**/*.{js,jsx,ts,tsx}", // Include all JS, JSX, TS, and TSX files in the components folder]
+    "./app/(tabs)/meditate.tsx",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        rmono: ["Roboto-Mono", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
