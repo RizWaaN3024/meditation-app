04596f062399d1e49be31bac34b2e07d6bd2f99c
RizWaaN3024 <rizwaanans786@gmail.com>
2024-07-09 03:00:04 +0530

    Code Fixes

---
+++ app/(tabs)/affirmations/_layout.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const Layout = () => {
+  return (
+    <View>
+      <Text>Layout</Text>
+    </View>
+  );
+};
+
+export default Layout;
