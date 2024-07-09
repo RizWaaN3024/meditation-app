261857f3ea52a1ecce7000c74df9b87f6437456a
RizWaaN3024 <rizwaanans786@gmail.com>
2024-07-10 01:36:40 +0530

    Affirmation Detail Screen INitial Setup

--- app/(tabs)/affirmations/[itemId].tsx
+++ app/(tabs)/affirmations/[itemId].tsx
@@ -1,12 +1,62 @@
-import { View, Text } from "react-native";
-import React from "react";
-import { useLocalSearchParams } from "expo-router";
+import {
+  View,
+  Text,
+  ImageBackground,
+  Pressable,
+  ScrollView,
+} from "react-native";
+import React, { useEffect, useState } from "react";
+import { useLocalSearchParams, router } from "expo-router";
+import { GalleryPreviewData } from "@/constants/models/AffirmationCategory";
+import AFFIRMATION_GALLERY from "@/constants/affirmation-gallery";
+import AppGradient from "@/components/AppGradient";
+import { AntDesign } from "@expo/vector-icons";
 
 const AffirmationPractice = () => {
   const { itemId } = useLocalSearchParams();
+
+  const [affirmation, setAffirmation] = useState<GalleryPreviewData>();
+
+  useEffect(() => {
+    for (let idx = 0; idx < AFFIRMATION_GALLERY.length; idx++) {
+      const affirmationData = AFFIRMATION_GALLERY[idx].data;
+
+      const affirmationToStart = affirmationData.find(
+        (a) => a.id === Number(itemId)
+      );
+
+      if (affirmationToStart) {
+        setAffirmation(affirmationToStart);
+
+        return;
+      }
+    }
+  }, []);
   return (
-    <View>
-      <Text>AffirmationPractice</Text>
+    <View className="flex-1">
+      <ImageBackground
+        source={affirmation?.image}
+        resizeMode="cover"
+        className="flex-1"
+      >
+        <AppGradient colors={["rgba(0, 0, 0, 0.3)", "rgba(0, 0, 0, 0.9)"]}>
+          <Pressable
+            onPress={() => router.back()}
+            className="absolute top-16 left-6 z-10"
+          >
+            <AntDesign name="leftcircleo" size={50} color="white" />
+          </Pressable>
+          <ScrollView className="mt-20" showsVerticalScrollIndicator={false}>
+            <View className="h-full justify-center">
+              <View className="h-4/5 justify-center">
+                <Text className="text-white text-3xl mb-12 font-bold text-center">
+                  {affirmation?.text}
+                </Text>
+              </View>
+            </View>
+          </ScrollView>
+        </AppGradient>
+      </ImageBackground>
     </View>
   );
 };
--- app/(tabs)/affirmations/_layout.tsx
+++ app/(tabs)/affirmations/_layout.tsx
@@ -6,6 +6,7 @@ const AffirmationsLayout = () => {
   return (
     <Stack>
       <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen name="[itemId]" options={{ headerShown: false }} />
     </Stack>
   );
 };
