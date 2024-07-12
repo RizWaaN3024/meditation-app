1d66413115f24702a8bee6a826a9aba94f9f6802
RizWaaN3024 <rizwaanans786@gmail.com>
2024-07-13 01:58:52 +0530

    Loaded Fonts and timer code fixes

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,22 @@
-import { Slot, Stack } from "expo-router";
+import { useFonts } from "expo-font";
+import { Slot, SplashScreen, Stack } from "expo-router";
+import { useEffect } from "react";
+
+// Prevents the splash Screen from autohiding until loading all the font assets
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  const [fontsLoaded, error] = useFonts({
+    "Roboto-Mono": require("../assets/fonts/RobotoMono-Regular.ttf")
+  })
+
+  useEffect(() => {
+    if (error) throw error;
+    if (fontsLoaded) SplashScreen.hideAsync();
+  }, [fontsLoaded, error])
+
+  if (!fontsLoaded) return null;
+  if (fontsLoaded && !error) return null;
   return (
     <Stack>
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
--- app/meditate/[id].tsx
+++ app/meditate/[id].tsx
@@ -30,6 +30,9 @@ const Meditate = () => {
       clearTimeout(timerId)
     }
   }, [secondsRemaining, isMeditating])
+
+  const formattedTimeMinutes = String(Math.floor(secondsRemaining / 60)).padStart(2, "0");
+  const formattedTimeSeconds = String(secondsRemaining % 60).padStart(2, "0");
   return (
     <View className="flex-1">
       <ImageBackground
@@ -47,7 +50,7 @@ const Meditate = () => {
 
           <View className="flex-1 justify-center">
             <View className="mx-auto bg-neutral-200 rounded-full w-44 h-44 justify-center items-center">
-              <Text className="text-4xl text-blue-800">00.{secondsRemaining}</Text>
+              <Text className="text-4xl text-blue-800 font-rmono">{formattedTimeMinutes}.{formattedTimeSeconds}</Text>
             </View>
           </View>
           <View className="mb-5">
