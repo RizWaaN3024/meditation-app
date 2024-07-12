7580a5fda6e16ca5fd736fdb76966184874f1ea8
RizWaaN3024 <rizwaanans786@gmail.com>
2024-07-13 01:42:18 +0530

    Added Timer Functionality in Mediatation Detail Screen

--- app/(tabs)/nature-meditate.tsx
+++ app/(tabs)/nature-meditate.tsx
@@ -27,7 +27,7 @@ const NatureMeditate = () => {
             showsVerticalScrollIndicator={false}
             renderItem={({ item }) => (
               <Pressable
-                onPress={() => router.push("/meditate")}
+                onPress={() => router.push(`/meditate/${item.id}`)}
                 className="h-48 my-3 rounded-md overflow-hidden"
               >
                 <ImageBackground
--- app/meditate/[id].tsx
+++ app/meditate/[id].tsx
@@ -1,15 +1,36 @@
 import { View, Text, ImageBackground, Pressable } from "react-native";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import MEDITATION_IMAGES from "@/constants/meditation-images";
 import AppGradient from "@/components/AppGradient";
-import { router } from "expo-router";
+import { router, useLocalSearchParams } from "expo-router";
 import { AntDesign } from "@expo/vector-icons";
+import CustomButton from "@/components/CustomButton";
 
 const Meditate = () => {
+  const { id } = useLocalSearchParams();
+
+  const [secondsRemaining, setSecondsRemaining] = useState<number>(20);
+  const [isMeditating, setIsMeditating] = useState(false);
+
+  useEffect(() => {
+    let timerId: NodeJS.Timeout;
+
+    // Exit 
+    if(secondsRemaining === 0) {
+      return;
+    } else {
+      timerId = setTimeout(() => {
+        setSecondsRemaining(secondsRemaining - 1);
+      }, 1000);
+    }
+    return () => {
+      clearTimeout(timerId)
+    }
+  }, [secondsRemaining])
   return (
     <View className="flex-1">
       <ImageBackground
-        source={MEDITATION_IMAGES[0]}
+        source={MEDITATION_IMAGES[Number(id) - 1]}
         resizeMode="cover"
         className="flex-1"
       >
@@ -20,6 +41,15 @@ const Meditate = () => {
           >
             <AntDesign name="leftcircleo" size={50} color="white" />
           </Pressable>
+
+          <View className="flex-1 justify-center">
+            <View className="mx-auto bg-neutral-200 rounded-full w-44 h-44 justify-center items-center">
+              <Text className="text-4xl text-blue-800">00.{secondsRemaining}</Text>
+            </View>
+          </View>
+          <View className="mb-5">
+            <CustomButton title="Start Meditation" onPress={() => console.log("Hello")} />
+          </View>
         </AppGradient>
       </ImageBackground>
     </View>
