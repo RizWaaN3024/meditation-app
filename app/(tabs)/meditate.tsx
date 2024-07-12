05786e5db880efdf8ff86dbee4e8e86165b1403b
RizWaaN3024 <rizwaanans786@gmail.com>
2024-07-13 01:23:31 +0530

    Meditation Screen

--- app/(tabs)/meditate.tsx
+++ app/(tabs)/meditate.tsx
@@ -1,12 +1,23 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, ImageBackground, Pressable } from "react-native";
+import React from "react";
+import MEDITATION_IMAGES from "@/constants/meditation-images";
+import AppGradient from "@/components/AppGradient";
+import { router } from "expo-router";
+import { AntDesign } from "@expo/vector-icons";
 
 const Meditate = () => {
   return (
-    <View>
-      <Text>Meditate</Text>
+    <View className="flex-1">
+      <ImageBackground source={MEDITATION_IMAGES[0]} resizeMode="cover" className="flex-1">
+        <AppGradient colors={["transparent", "rgba(0, 0, 0, 0.8)"]}>
+            <Pressable onPress={() => router.back()} className="absolute top-16 left-6 z-10">
+            <AntDesign name="leftcircle" size={50} color="white" />
+            </Pressable>
+        <Text>Meditate</Text>
+        </AppGradient>
+      </ImageBackground>
     </View>
-  )
-}
+  );
+};
 
-export default Meditate
+export default Meditate;
